0f04b2c2d49316af198c1fc0774d33b8e099a90e
Nazira17 <you@example.com>
2019-12-13 11:10:12 +0600

    Add old change

--- test.py
+++ test.py
@@ -1,34 +1,37 @@
-from appium import webdriver
-import os
+import pytest
 import time
 
 
-desired_cap = {}
-desired_cap["deviceName"] = "1283b2ca"
-desired_cap["platformName"] = "Android"
-desired_cap["app"] = os.path.abspath(
-    os.path.abspath
-    ("/home/nazira/test_project/hck-2.1.58_final.apk"))
-desired_cap["appPackage"] = "kz.homecredit.ibank"
-desired_cap["appActivity"] = "cz.bsc.mb.activities.prelogin.SplashScreenActivity"
-driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_cap)
-# driver.quit()
-desired_cap["newCommandTimeout"] = 6000
-# driver.set_page_load_timeout(6000)
+# desired_cap = {}
+# desired_cap["deviceName"] = "1283b2ca"
+# desired_cap["platformName"] = "Android"
+# desired_cap["app"] = os.path.abspath(
+#     os.path.abspath
+#     ("/home/nazira/test_project/hck-2.1.58_final.apk"))
+# desired_cap["appPackage"] = "kz.homecredit.ibank"
+# desired_cap["appActivity"] = "cz.bsc.mb.activities.prelogin.SplashScreenActivity"
+# driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_cap)
+# # driver.quit()
+# desired_cap["newCommandTimeout"] = 6000
+# # driver.set_page_load_timeout(6000)
 
-path = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.EditText"
-search_element = driver.find_element_by_xpath(path)
-search_element.send_keys("7071119966")
-button_send = driver.find_element_by_id("kz.homecredit.ibank:id/customButton")
-button_send.click()
-driver.implicitly_wait(30)
-path2 = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.EditText"
-search_element_password = driver.find_element_by_xpath(path2)
-search_element_password.send_keys("123QWEasd")
-button_send = driver.find_element_by_id("kz.homecredit.ibank:id/customButton")
-button_send.click()
-time.sleep(10)
-print(2)
+def test_connect_app(data):
+    driver, desired_cap = data
+    path = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.EditText"
+    search_element = driver.find_element_by_xpath(path)
+    search_element.send_keys("7071119966")
+    assert driver.find_element_by_xpath(path) == path
+    assert driver.send_keys('701119966') == 701119966
+    button_send = driver.find_element_by_id(
+        "kz.homecredit.ibank:id/customButton").click()
+    driver.implicitly_wait(30)
+    path2 = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.EditText"
+    search_element_password = driver.find_element_by_xpath(
+        path2).send_keys("123QWEasd")
+    button_send = driver.find_element_by_id(
+        "kz.homecredit.ibank:id/customButton").click()
+    time.sleep(10)
+    return button_send, search_element_password
 
 
 
