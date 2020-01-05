8a673446da8ad4907a4199a990197ab1e02d9c14
Nazira17 <you@example.com>
2020-01-05 16:09:39 +0600

    Initial commit

--- test_transfer.py
+++ test_transfer.py
@@ -4,13 +4,12 @@ from . decorators import transfer
 
 @transfer
 def test_transfer_between_owned(driver):
-    print(1)
     # driver.find_element_by_id("kz.homecredit.ibank:id/mnuTransfers").click()
     # time.sleep(5)
     driver.find_element_by_android_uiautomator(
         'new UiSelector().text("Между своими счетами")'
     ).click()
-    time.sleep(5)
+    time.sleep(2)
     driver.find_element_by_android_uiautomator(
         'new UiSelector().text("Счет списания")'
     ).click()
@@ -35,103 +34,164 @@ def test_transfer_between_owned(driver):
         'android.widget.LinearLayout[3]/android.widget.LinearLayout/'
         'android.widget.EditText'
     )
-    driver.find_element_by_xpath(path).send_keys("1000")
+    driver.find_element_by_xpath(path).send_keys("50")
     driver.find_element_by_id(
         "kz.homecredit.ibank:id/payButton"
     ).click()
+    time.sleep(3)
+    element = driver.find_element_by_id(
+        'kz.homecredit.ibank:id/title')
+    text = element.text
+    assert text == "Транзакция успешно обработана"
     driver.find_element_by_id(
         "kz.homecredit.ibank:id/button1"
     ).click()
     driver.find_element_by_accessibility_id(
         "Перейти вверх"
     ).click()
+
+
+@transfer
+def test_transfer_between_owned_deposit(driver):
+    # driver.find_element_by_id("kz.homecredit.ibank:id/mnuTransfers").click()
+    # time.sleep(5)
+    print(1)
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Между своими счетами")'
+    ).click()
+    time.sleep(2)
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Счет списания")'
+    ).click()
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Текущий счет")'
+    ).click()
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("На счёт")'
+    ).click()
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Депозит")'
+    ).click()
+
+    path = (
+        '/hierarchy/android.widget.FrameLayout/'
+        'android.widget.LinearLayout/android.widget.FrameLayout/'
+        'android.widget.FrameLayout/android.widget.FrameLayout/'
+        'android.view.ViewGroup/android.widget.FrameLayout/'
+        'android.widget.RelativeLayout/android.widget.RelativeLayout/'
+        'android.widget.ScrollView/android.widget.LinearLayout/'
+        'android.widget.FrameLayout/android.widget.LinearLayout/'
+        'android.widget.LinearLayout[3]/android.widget.LinearLayout/'
+        'android.widget.EditText'
+    )
+    driver.find_element_by_xpath(path).send_keys("50")
+    driver.find_element_by_id(
+        "kz.homecredit.ibank:id/payButton"
+    ).click()
     time.sleep(3)
-    print(2)
+    element = driver.find_element_by_id(
+        'kz.homecredit.ibank:id/title')
+    text = element.text
+    assert text == "Транзакция успешно обработана"
+    driver.find_element_by_id(
+        "kz.homecredit.ibank:id/button1"
+    ).click()
+    driver.find_element_by_accessibility_id(
+        "Перейти вверх"
+    ).click()
 
 
-# def test_transfer_between_owned_deposit(driver, back_transfer):
-#     # driver.find_element_by_id("kz.homecredit.ibank:id/mnuTransfers").click()
-#     # time.sleep(5)
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("Между своими счетами")'
-#     ).click()
-#     time.sleep(5)
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("Счет списания")'
-#     ).click()
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("Текущий счет")'
-#     ).click()
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("На счёт")'
-#     ).click()
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("Депозит")'
-#     ).click()
+@transfer
+def test_transfer_between_owned_deposit_to_account(driver):
+    # driver.find_element_by_id("kz.homecredit.ibank:id/mnuTransfers").click()
+    # time.sleep(5)
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Между своими счетами")'
+    ).click()
+    time.sleep(2)
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Счет списания")'
+    ).click()
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Депозит")'
+    ).click()
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("На счёт")'
+    ).click()
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Текущий счет")'
+    ).click()
 
-#     path = (
-#         '/hierarchy/android.widget.FrameLayout/'
-#         'android.widget.LinearLayout/android.widget.FrameLayout/'
-#         'android.widget.FrameLayout/android.widget.FrameLayout/'
-#         'android.view.ViewGroup/android.widget.FrameLayout/'
-#         'android.widget.RelativeLayout/android.widget.RelativeLayout/'
-#         'android.widget.ScrollView/android.widget.LinearLayout/'
-#         'android.widget.FrameLayout/android.widget.LinearLayout/'
-#         'android.widget.LinearLayout[3]/android.widget.LinearLayout/'
-#         'android.widget.EditText'
-#     )
-#     driver.find_element_by_xpath(path).send_keys("50")
-#     driver.find_element_by_id(
-#         "kz.homecredit.ibank:id/payButton"
-#     ).click()
-#     driver.find_element_by_id(
-#         "kz.homecredit.ibank:id/button1"
-#     ).click()
-#     driver.find_element_by_accessibility_id(
-#         "Перейти вверх"
-#     ).click()
-#     time.sleep(3)
+    path = (
+        '/hierarchy/android.widget.FrameLayout/'
+        'android.widget.LinearLayout/android.widget.FrameLayout/'
+        'android.widget.FrameLayout/android.widget.FrameLayout/'
+        'android.view.ViewGroup/android.widget.FrameLayout/'
+        'android.widget.RelativeLayout/android.widget.RelativeLayout/'
+        'android.widget.ScrollView/android.widget.LinearLayout/'
+        'android.widget.FrameLayout/android.widget.LinearLayout/'
+        'android.widget.LinearLayout[3]/android.widget.LinearLayout/'
+        'android.widget.EditText'
+    )
+    driver.find_element_by_xpath(path).send_keys("50")
+    driver.find_element_by_id(
+        "kz.homecredit.ibank:id/payButton"
+    ).click()
+    time.sleep(3)
+    element = driver.find_element_by_id(
+        'kz.homecredit.ibank:id/title')
+    text = element.text
+    assert text == "Транзакция успешно обработана"
+    driver.find_element_by_id(
+        "kz.homecredit.ibank:id/button1"
+    ).click()
+    driver.find_element_by_accessibility_id(
+        "Перейти вверх"
+    ).click()
 
 
-# def test_transfer_between_owned_deposit_to_account(driver, back_transfer):
-#     # driver.find_element_by_id("kz.homecredit.ibank:id/mnuTransfers").click()
-#     # time.sleep(5)
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("Между своими счетами")'
-#     ).click()
-#     time.sleep(5)
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("Счет списания")'
-#     ).click()
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("Депозит")'
-#     ).click()
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("На счёт")'
-#     ).click()
-#     driver.find_element_by_android_uiautomator(
-#         'new UiSelector().text("Текущий счет")'
-#     ).click()
+@transfer
+def test_transfer_between_owned_deposit_to_cart(driver):
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Между своими счетами")'
+    ).click()
+    time.sleep(2)
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Счет списания")'
+    ).click()
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Депозит")'
+    ).click()
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("На счёт")'
+    ).click()
+    driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("Дебетная карта")'
+    ).click()
 
-#     path = (
-#         '/hierarchy/android.widget.FrameLayout/'
-#         'android.widget.LinearLayout/android.widget.FrameLayout/'
-#         'android.widget.FrameLayout/android.widget.FrameLayout/'
-#         'android.view.ViewGroup/android.widget.FrameLayout/'
-#         'android.widget.RelativeLayout/android.widget.RelativeLayout/'
-#         'android.widget.ScrollView/android.widget.LinearLayout/'
-#         'android.widget.FrameLayout/android.widget.LinearLayout/'
-#         'android.widget.LinearLayout[3]/android.widget.LinearLayout/'
-#         'android.widget.EditText'
-#     )
-#     driver.find_element_by_xpath(path).send_keys("50")
-#     driver.find_element_by_id(
-#         "kz.homecredit.ibank:id/payButton"
-#     ).click()
-#     driver.find_element_by_id(
-#         "kz.homecredit.ibank:id/button1"
-#     ).click()
-#     driver.find_element_by_accessibility_id(
-#         "Перейти вверх"
-#     ).click()
-#     time.sleep(3)
+    path = (
+        '/hierarchy/android.widget.FrameLayout/'
+        'android.widget.LinearLayout/android.widget.FrameLayout/'
+        'android.widget.FrameLayout/android.widget.FrameLayout/'
+        'android.view.ViewGroup/android.widget.FrameLayout/'
+        'android.widget.RelativeLayout/android.widget.RelativeLayout/'
+        'android.widget.ScrollView/android.widget.LinearLayout/'
+        'android.widget.FrameLayout/android.widget.LinearLayout/'
+        'android.widget.LinearLayout[3]/android.widget.LinearLayout/'
+        'android.widget.EditText'
+    )
+    driver.find_element_by_xpath(path).send_keys("50")
+    driver.find_element_by_id(
+        "kz.homecredit.ibank:id/payButton"
+    ).click()
+    time.sleep(3)
+    element = driver.find_element_by_id(
+        'kz.homecredit.ibank:id/title')
+    text = element.text
+    assert text == "Транзакция успешно обработана"
+    driver.find_element_by_id(
+        "kz.homecredit.ibank:id/button1"
+    ).click()
+    driver.find_element_by_accessibility_id(
+        "Перейти вверх"
+    ).click()
